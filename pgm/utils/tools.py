85ed4dd4146c7932320d4d95bc88bbe4086ed96c
Argentina Ortega Sáinz <argentina.ortega@smail.inf.h-brs.de>
2015-07-23 21:01:52 +0200

    Cleaned up utils functions

--- pgm/utils/tools.py
+++ pgm/utils/tools.py
@@ -45,8 +45,9 @@ def node_types(graph):
         elif graph.node[n]['type'] == 'utility':
             utility.append(n)
 
-    return chance, decision, utility
+    nodes = {'chance':chance, 'decision':decision, 'utility':utility}
 
+    return nodes
 
 def node_type(n):
     return n['type']
@@ -55,27 +56,29 @@ def reverse_edge(graph, edge):
     #graph.remove_edge(edge[0], edge[1])
     graph.add_edge(edge[1], edge[0])
 
-def draw_graph(graph, show=False, save=False):
-    # 's' = square
-    # 'D' = diamond
-    # 'o' = circle
-
+def draw_graph(graph, pos=None, size=600, alpha=0.9, show=False, save=False):
     title = graph.graph['title']
 
     plt.figure()
+    plt.axis('off')
 
-    chance, decision, utility = node_types(graph)
-    pos = nx.graphviz_layout(graph, prog='dot')
+    if pos==None:
+        p = nx.graphviz_layout(graph, prog='dot')
+    else:
+        p=pos
+
+    node_dict = node_types(graph)
+    chance, decision, utility = node_dict.get('chance'), node_dict.get('decision'), node_dict.get('utility')
 
     if len(chance) > 0:
-        nx.draw_networkx_nodes(graph, pos, nodelist=chance, node_size=200)
+        nx.draw_networkx_nodes(graph, p, nodelist=chance, node_size=size, node_shape='o', alpha=alpha, node_color='w')
     if len(decision) > 0:
-        nx.draw_networkx_nodes(graph, pos, nodelist=decision, node_size=200, node_shape='s')
+        nx.draw_networkx_nodes(graph, p, nodelist=decision, node_size=size, node_shape='s',alpha=alpha, node_color='g')
     if len(utility) > 0:
-        nx.draw_networkx_nodes(graph, pos, nodelist=utility, node_size=200, node_shape='D')
+        nx.draw_networkx_nodes(graph, p, nodelist=utility, node_size=size, node_shape='D', alpha=alpha, node_color='r')
 
-    nx.draw_networkx_edges(graph, pos=pos)
-    nx.draw_networkx_labels(graph, pos=pos)
+    nx.draw_networkx_edges(graph, pos=p)
+    nx.draw_networkx_labels(graph, pos=p)
 
     if save:
         nx.write_dot(graph, title + '.dot')
