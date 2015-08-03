9a2079a16aa486693c498246da598ae0c997686a
Argentina Ortega Sáinz <argentina.ortega@smail.inf.h-brs.de>
2015-08-03 17:06:48 +0200

    Changed appearance of junction trees

--- pgm/utils/tools.py
+++ pgm/utils/tools.py
@@ -138,14 +138,14 @@ def draw_graph(graph, pos=None, size=600, alpha=0.9, show=False, save=False):
                 edge_labels.append(((u, v),
                                     'in: u=%(u_in).2f p=%(p_in).2f\nout: u=%(u_out).2f p=%(p_out).2f\n%(separator)s'
                                     % graph.get_edge_data(u, v)))
-            nx.draw_networkx(graph, pos=p, node_size=900)
+            nx.draw_networkx(graph, pos=p, node_size=900, node_color='w')
             nx.draw_networkx_edge_labels(graph, pos=p, edge_labels=dict(edge_labels),
                                          bbox=dict(boxstyle='square', fc='w', ec='k'), rotate=False)
 
         elif graph.graph['type'] is 'join_tree':
             print 'join_tree'
-            nx.draw_networkx(graph, pos=p)
-            nx.draw_networkx_edge_labels(graph, pos=p)
+            nx.draw_networkx(graph, pos=p, node_size=900, node_color='w')
+            #nx.draw_networkx_edge_labels(graph, pos=p)
     else:
         print 'other'
         nx.draw_networkx_edges(graph, pos=p)
